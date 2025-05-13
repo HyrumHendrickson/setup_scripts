6f57268baad911d97d3e82bdf1c51360feff7a07
Hyrum Hendrickson <94187900+HyrumHendrickson@users.noreply.github.com>
2025-05-13 19:50:52 +0000

    worked on script

--- setup_quarto.sh
+++ setup_quarto.sh
@@ -1,26 +1,55 @@
 #!/bin/bash
 
-# --- This script is meant to setup quarto documents for R and python in VS code ---
+# --- This script is meant to setup quarto documents for R, Python, and SQL in VS code ---
+# --- to use; run -> curl xxxx ---
 
 # basic update
-sudo apt update
+sudo apt update -y
 
 # install R
-sudo apt install r-base
+sudo apt install -y r-base
 
 # install quarto
 wget https://quarto.org/download/latest/quarto-linux-amd64.deb
 sudo dpkg -i quarto-linux-amd64.deb
 rm quarto-linux-amd64.deb
 
+# install SQL
+sudo apt install sqlite3
+
 # install R extension
-code --install-extension ikuyadeu.r
+code --install-extension REditorSupport.r
 
 # install quarto extension
 code --install-extension quarto.quarto
 
+# install SQL extension
+
+# install jupyter extension
+
+# install misc data extensions
+
+# setup python venv
+python3 -m venv .venv
+source ".venv/bin/activate"
+
+# setup r env
+cat <<EOL > init_renv.R
+install.packages("renv", repos = "https://cloud.r-project.org")
+renv::init(bare = TRUE)
+EOL
+Rscript init_renv.R
+rm init_renv.R
+
 # install python packages
+#pip install numpy pandas matplotlib seaborn scikit-learn scipy jupyter notebook plotly sqlite3 ipykernel
 
 # install R packages
+#sudo Rscript -e "install.packages(c('languageserver', 'pander', 'tidyverse', 'quarto', 'knitr', 'rmarkdown'), repos='https://cloud.r-project.org')"
 
-# logic for optional R and python envorinments 
+# dowload example files
+# grabs a qmd
+# grabs a juptyer notebook
+# grabs a sqllite file
+# grabs a csv
+# grabs a json
